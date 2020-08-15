9a357a0bbdbbb554448a76281f51a6b3435fd8d3
Pravin Patel <patelpra337@gmail.com>
2020-08-15 11:32:23 -0500

    Create new project storyboard and organize

---
+++ Pokedex-Objc/Pokedex-Objc/Resources/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Pokedex-Objc
+//
+//  Created by patelpra on 8/15/20.
+//  Copyright © 2020 Crus Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Pokedex-Objc/Pokedex-Objc/Resources/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Pokedex-Objc
+//
+//  Created by patelpra on 8/15/20.
+//  Copyright © 2020 Crus Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
